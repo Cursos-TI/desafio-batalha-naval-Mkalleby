3abc44acc6f1443b20781e56b5ed876ec3c9ed83
Mkalleby <marcoskalleby035@gmail.com>
2025-02-08 16:29:56 +0000

    aventureiro 50%

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,27 +3,53 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
-
+#define LINHAS 10
+#define COLUNAS 10
+#define BARCOS 4
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-    int tabuleiro[5][5] = {
-        {0, 0, 0, 1, 0},
-        {0, 0, 0, 1, 0},
-        {0, 0, 0, 1, 0},
-        {0, 0, 0, 0, 0},
-        {2, 2, 2, 0, 0}
-    };
-
-        printf("a posição do barco 1 é: %d, %d, %d;\n", tabuleiro[0][3], tabuleiro[1][3], tabuleiro[2][3]);
-        printf("a posição do barco 2 é: %d, %d, %d;\n", tabuleiro[4][0], tabuleiro[4][1], tabuleiro[4][2]);
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int tabuleiro2[LINHAS][COLUNAS] = {
+        
+    };
+
+    int sem_barco = 0;
+    int com_barco1 = 3;
+    int com_barco2 = 3;
+    int com_barco3 = 3;
+
+    for (int i = 0; i < LINHAS; i++)
+    {
+        for (int j = 0; j < COLUNAS; j++)
+        {
+            tabuleiro2[i][j] = sem_barco;
+            
+            tabuleiro2[9][0] = com_barco3;
+            tabuleiro2[8][1] = com_barco3;
+            tabuleiro2[7][2] = com_barco3;
+            tabuleiro2[6][3] = com_barco3;
+            
+            for (int k = 0, l = 0; k < BARCOS, l < BARCOS; k++, l++)
+            {
+                tabuleiro2[k][5] = com_barco1;
+                tabuleiro2[4][l] = com_barco2;
+            }
+            
+            printf("%d ", tabuleiro2[i][j]);
+        }
+        printf("\n");
+    }
+    
+
+
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
